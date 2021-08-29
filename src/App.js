0622244e1e9a4ac1171e51c29f9a11ee89583cb4
zainabts <zainabts@aucegypt.edu>
2021-08-29 13:21:30 +0200

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ListGroup } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>App Component</h1>
+        <ListGroup as="ul">
+          <ListGroup.Item as="li" active>
+            First Item
+          </ListGroup.Item>
+          <ListGroup.Item as="li">Second Item</ListGroup.Item>
+          <ListGroup.Item as="li" disabled>
+            Third Item
+          </ListGroup.Item>
+          <ListGroup.Item as="li">Fourth Item</ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 }
